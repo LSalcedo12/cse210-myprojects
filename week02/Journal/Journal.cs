8112e1f70f1f0eed941992b80b884f28d0b1403f
Luisa Salcedo <valeriaslsn@gmail.com>
2026-09-12 17:08:02 -0600

    Finished

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -28,7 +28,7 @@ public class Journal
         {
             foreach (Entry entry in _entries)
             {
-                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+                outputFile.WriteLine($"{entry._date}|{entry._mood}|{entry._promptText}|{entry._entryText}");
             }
         }
         Console.WriteLine("Journal saved successfully!\n");
@@ -49,12 +49,13 @@ public class Journal
         foreach (string line in lines)
         {
             string[] parts = line.Split('|');
-            if (parts.Length == 3)
+            if (parts.Length == 4)
             {
                 Entry entry = new Entry();
                 entry._date = parts[0];
-                entry._promptText = parts[1];
-                entry._entryText = parts[2];
+                entry._mood = parts[1];
+                entry._promptText = parts[2];
+                entry._entryText = parts[3];
                 _entries.Add(entry);
             }
         }
