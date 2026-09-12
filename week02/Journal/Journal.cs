54191b446d3d8fe32a7c057ce079fe7f83e585ab
Luisa Salcedo <valeriaslsn@gmail.com>
2026-09-12 16:27:33 -0600

    Finished

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -6,6 +6,22 @@ public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
 
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+    public void DisplayAll()
+    {
+        if (_entries.Count == 0)
+        {
+            Console.WriteLine("No entries found in current journal.\n");
+            return;
+        }
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
     public void SaveToFile(string file)
     {
         using (StreamWriter outputFile = new StreamWriter(file))
@@ -15,6 +31,33 @@ public class Journal
                 outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
             }
         }
-        Console.WriteLine("File saved successfully!");
+        Console.WriteLine("Journal saved successfully!\n");
+    }
+
+    public void LoadFromFile(string file)
+    {
+        if (!File.Exists(file))
+        {
+            Console.WriteLine("File not found.\n");
+            return;
+        }
+
+        _entries.Clear();
+        
+        string[] lines = File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            if (parts.Length == 3)
+            {
+                Entry entry = new Entry();
+                entry._date = parts[0];
+                entry._promptText = parts[1];
+                entry._entryText = parts[2];
+                _entries.Add(entry);
+            }
+        }
+        Console.WriteLine("Journal loaded successfully!\n");
     }
 }
